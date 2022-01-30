9b7e074901dcf88feb516b7c87ce3a052b9bc560
Abdullah <umar.abd02@hotmail.com>
2022-01-30 17:45:42 +0500

    Add project files.

---
+++ Helper_Classes/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper_Classes
+{
+    public class Car
+    {
+        public string Owner { get; set; }
+        public string Reg_num { get; set; } 
+        public int Distance
+        {
+            set
+            {
+                if (value == 50 || value == 100 || value == 200)
+                {
+                    Distance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Value");
+                }
+            }
+
+            get
+            {
+                return Distance;
+            }
+        }
+
+        public Car(string own = "", string reg = "", int dist = 0)
+        {
+            Owner = own;
+            Reg_num = reg;
+            Distance = dist;
+        }
+
+    }
+}
---
+++ Mechanic_Problem/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Helper_Classes;
+
+
+
+namespace Main
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+            
+            Console.ReadKey();
+        }
+    }
+}
